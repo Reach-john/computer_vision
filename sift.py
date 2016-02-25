a20ae2ea64509d35759ff1948b20227ea482d324
Reach John <326713510@qq.com>
2016-02-25 21:51:46 +0800

    new files

--- sift.py
+++ sift.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from os import system
-from numpy import loadtxt, savetxt, hstack, arange, pi, cos, sin
+from numpy import loadtxt, savetxt, hstack, arange, pi, cos, sin, array, linalg, zeros, dot, argsort, arccos
 from pylab import plot, imshow, axis
 
 
@@ -36,3 +36,29 @@ def plot_feature(im, locs, circle=False):
         plot(locs[:, 0], locs[:, 1], 'ob')
     axis('off')
     return
+
+def match(desc1, desc2):
+    desc1 = array(d/linalg.norm(d) for d in desc1)
+    desc2 = array(d/linalg.norm(d) for d in desc2)
+
+    dist_ratio = 0.6
+    desc1_size = desc1.shape
+
+    matchscores = zeros((desc1_size[0], 1), 'int')
+    desc2t = desc2.T
+    for i in range(desc1_size):
+        dotprods = dot(desc1[i, :], desc2t)
+        dotprods = 0.999*dotprods
+        indx = argsort(arccos(dotprods))
+        if arccos(dotprods)[indx[0]] < dist_ratio*arccos(dotprods)[indx[1]]:
+            matchscores[i] = int(indx[0])
+    return matchscores
+
+def match_twosided(desc1, desc2):
+    match12 = match(desc1, desc2)
+    match21 = match(desc2, desc1)
+    ndx_12 = match12.nonzero()[0]
+    for n in ndx_12:
+        if match21[int(match12[n])] != n:
+            match12[n] = 0
+    return match12
